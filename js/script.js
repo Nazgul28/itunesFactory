e2b82df99dc5f82ace43bb96e2cf66ac5cc8a5a0
Nazgul28 <lfmassola@gmail.com>
2016-01-27 22:29:31 -0800

    appending jQuery + css

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 $(function () {
-  
+
   // console.log('hi');
   
   $('#album-search').submit(function(event){
-     event.preventDefault();
-  
+   event.preventDefault();
+
 
    console.log('hi there');
-    
 
-    var artist = $('#artist-name').val();
-    var results = {};
-    var newLine = '';
 
-    $.ajax ({
+   var artist = $('#artist-name').val();
+   var results = {};
+   var newLine = '';
+
+   $.ajax ({
     method: 'GET',
     url: 'https://itunes.apple.com/search?entity=album&limit=6&term=' + artist,
     dataType: 'jsonp'
-    })
+  })
 
-     .done(function(results){
+   .done(function(results){
             // var result = results.data;
             console.log(results);
 
             $(results.results).each(function(index,value) {
               // console.log(value, results);
-              console.log(value);
-
-            //   newLine += '<li>';
-            //   newLine += '<h3>' + results.data.collectionCensoredName + '</h3>';
-            //   newLine += '</li>';
-
-
-
+              // console.log(value.artistName);
+              newLine += '<li>';
+              newLine += '<h4>' + value.collectionName + '</h4></br>';
+              newLine += '<img src=" ' + value.artworkUrl100 + ' "/></br>';
+              newLine += '<h6>' + value.copyright + '</h6>';
+              newLine += '<p> $' + value.collectionPrice + '</p>';
+              newLine += '<a href=" ' + value.collectionViewUrl + ' "> check more on iTunes </a>';
+              newLine += '</li>';
 
             });
 
-            // $('.result-list').append(newLine);
+            $('.result-list').append(newLine);
+          });
 
-      });
-
-
-
-
-
+   $('#album-search').submit(function() {
+    $('.result-list').empty();
   });
+
+ });
+
 })
